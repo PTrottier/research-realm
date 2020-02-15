85653a44b7d6c7908475dad72c66a24a62c579b7
Patrick Trottier <PTrottier@laurentian.ca>
2020-02-15 01:26:24 -0500

    Fix Formatting

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Research Realm</title>
-	</head>
-	<body>
-      <header>
-        <nav>
-          <a href="#">
-            <img alt="Research Realm" src="logo.png" />
-          </a>
-          <div id="filter">
-            <form method="POST" action="/">
-              <label for="department">Department/Département</label>
-              <select id="department">
-                <option value="cosc">Computer Science / Sciences Informatiques</option>
-                <option value="kin">Kinesiology / Kinésiologie</option>
-                <option value="psych">Psychology / Psychologie</option>
-              </select>
-              <input type="reset" value="Reset" />
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
+  <head>
+    <title>Research Realm</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <a href="#">
+          <img alt="Research Realm" src="logo.png" />
+        </a>
+        <div id="filter">
+          <form method="POST" action="/">
+            <label for="department">Department/Département</label>
+            <select id="department">
+              <option value="cosc">Computer Science / Sciences Informatiques</option>
+              <option value="kin">Kinesiology / Kinésiologie</option>
+              <option value="psych">Psychology / Psychologie</option>
+            </select>
+            <input type="reset" value="Reset" />
+            <input type="submit" value="Submit" name="department" />
+          </form>
+        </div>
         </nav>
       </header>
-      <section>
+      <section id="projects">
       <?php
         $title = "Title";
         $department = "Department";
@@ -31,23 +31,26 @@
         for ($i = 0; $i < 4; $i++)
         {
             project($department . $i, $title . $i, $call_for_action . $i);
-            print '<hr />';
+            print "<hr />\n";
         }
         project($department . 5, $title . 5, $call_for_action . 5);
       ?>
       <?php function project($department, $title, $call_for_action) 
       { 
       ?>
-        <a href="#">
+
+        <a href="project.php">
           <article>
             <h2><?php print $title ?></h2>
             <h3><?php print $department ?></h3>
             <p><?php print $call_for_action ?></p>
           </article>
         </a>
+
       <?php 
       } 
       ?>
+
       </section>
-	</body>
+  </body>
 </html>
